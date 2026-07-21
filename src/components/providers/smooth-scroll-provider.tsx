6083b2bc664c78bebebd201b69e1e88488ce3103
alliancestreetgoa-lang <alliancestreetgoa@gmail.com>
2026-07-21 12:42:39 +0530

    Wire Lenis smooth scroll, LazyMotion, Navbar, and Footer into root layout

---
+++ src/components/providers/smooth-scroll-provider.tsx
@@ -0,0 +1,28 @@
+// src/components/providers/smooth-scroll-provider.tsx
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1,
+      easing: (t: number) => 1 - Math.pow(1 - t, 3),
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    const frame = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(frame);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
